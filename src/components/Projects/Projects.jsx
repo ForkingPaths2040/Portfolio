83d202e81eeed6615dd8f74c9f00feee69b557df
Amber Moroe <ambermoorexc@gmail.com>
2020-12-12 19:59:07 -0600

    Feature project of three styled/linked inside carousel. Material-Ui, Font-Awesome installed

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -4,6 +4,14 @@ import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-re
 import 'pure-react-carousel/dist/react-carousel.es.css';
 import pointual from '../../assets/images/pointualDetail.png'
 import progression from '../../assets/images/progressionTricks.png'
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LaunchIcon from '@material-ui/icons/Launch';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faJsSquare, faHtml5, faCss3, faReact } from '@fortawesome/free-brands-svg-icons'
+import { faTrain, faServer, faKey } from "@fortawesome/free-solid-svg-icons";
+
 
 function Projects(props) {
   return (
@@ -15,15 +23,31 @@ function Projects(props) {
         totalSlides={3}
       >
         <Slider>
-          <Slide index={0}>
-      <img className="pointual" src={pointual} />
+            <Slide index={0}>
+              <div className="project-feature">
+                <div className="project-icons">
+                  <a href="https://github.com/ForkingPaths2040/Pointual" target="_blank"><GitHubIcon style={{ fontSize: "32px" }}/></a>
+                  <a href="https://pointual.netlify.app/login" target="_blank"><LaunchIcon style={{ fontSize: "32px" }}/></a>
+                </div>
+                <img className="project-img" src={pointual} />
+                </div>
       <div className="description-container">
-        <h3>Pointual</h3>
-        <p className="project-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit pariatur culpa, quae dolore dicta, mollitia fuga veniam est, totam delectus dolores dolorem dolor velit officiis quidem maiores sequi laboriosam eius.</p>
-      </div>
+        <h3>Pointual <span id="project-label">| Full Stack via React on Rails</span></h3>
+        <p className="project-description">A full-stack productivity tool that allows users to track points against a time and attendance policy.</p>
+        <div className="toolkit-icons">
+                  <FontAwesomeIcon icon={faJsSquare} size="2x" style={{ color: "rgb(143, 143, 143)" }}/>
+                  <FontAwesomeIcon icon={faHtml5} size="2x"  style={{ color: "rgb(143, 143, 143)" }}/>
+                  <FontAwesomeIcon icon={faCss3} size="2x" style={{ color: "rgb(143, 143, 143)" }}/>
+                  <FontAwesomeIcon icon={faReact} size="2x" style={{ color: "rgb(143, 143, 143)" }}/>
+                  <FontAwesomeIcon icon={faTrain} size="2x" style={{ color: "rgb(143, 143, 143)" }}/>
+                  <FontAwesomeIcon icon={faServer} size="2x" style={{ color: "rgb(143, 143, 143)" }}/>
+                  <FontAwesomeIcon icon={faKey} size="2x" style={{ color: "rgb(143, 143, 143)" }}/>
+                  
+        </div>
+              </div>
           </Slide>
           <Slide index={1}>
-      <img className="pointual" src={progression} />
+      <img className="project-img" src={progression} />
       <div className="description-container">
         <h3>Progression</h3>
         <p className="project-description">Amber ipsum dolor sit amet, consectetur adipisicing elit. Odit pariatur culpa, quae dolore dicta, mollitia fuga veniam est, totam delectus dolores dolorem dolor velit officiis quidem maiores sequi laboriosam eius.</p>
@@ -31,8 +55,8 @@ function Projects(props) {
           </Slide>
           </Slider>
           <div className="page-buttons">
-          <ButtonBack>Back</ButtonBack>
-            <ButtonNext>Next</ButtonNext>
+            <ButtonBack className="carousel-button"><ChevronLeftIcon/></ButtonBack>
+            <ButtonNext className="carousel-button"><ChevronRightIcon/></ButtonNext>
             </div>
       </CarouselProvider>
       </div>
