fedc8fc50d51b9d7cbad93e95ce63fe5788df379
Amber Moroe <ambermoorexc@gmail.com>
2020-12-13 22:18:50 -0600

    Fixed warning of "commenting in jsx" to object literal

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -8,7 +8,7 @@ function Landing(props) {
         <h1 id="first-line"><span id="hello">Hello,</span> my name is Amber Moore.</h1>
       <div id="second-line">
         <h1>I am a software engineer specializing in full-stack applications</h1>
-        <h1 id="commented">// one line at a time.</h1>
+          <h1 id="commented">{`// one line at a time.`}</h1>
       </div>
       <div id='monoton'>
         <h1>LANDING &nbsp; PAGE</h1>
