69f05f342fb027d2e05845a9ed3a962df1d55831
Amber Moroe <ambermoorexc@gmail.com>
2020-12-21 04:40:50 -0600

    animated circle

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,6 +8,7 @@ function About(props) {
       <div className="about-info">
         <p>“I am a junior full-stack developer offering seven years of management experience in customer service.  With the exponential growth of advanced technology, I have chosen to change careers as I am adamant to be self-sufficient with society’s evolving norms. Since I approach problems systematically and have an appetite for expanding and challenging my intellect, web development is only the beginning in this vast, curious career field.  Ultimately, I have a passion for solving problems ‘inside-the-box’ as in most cases there are limitations from the client, a project or an organization. My ability to adapt and serve my own expectations provides a seamless addition to any project or organization.”</p>
       </div>
+      <div className="box"><div className="dash"></div></div>
       <div className="horizontal" id="hz1"></div>
       <div className="horizontal" id="hz2"></div>
       <div className="horizontal" id="hz3"></div>
